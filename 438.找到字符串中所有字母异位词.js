fa6f7177faf44505c05ca64275234addf7d69bbf
第四天灾 <luj850949@gmail.com>
2026-04-09 09:45:33 +0800

    不使用双数组维护子字符串，使用dif来维护两个字串不同数量字母的差值，通过dif是否为零判断是否满足条件。优化时间复杂度。修改“[438] 找到字符串中所有字母异位词”

--- 438.找到字符串中所有字母异位词.js
+++ 438.找到字符串中所有字母异位词.js
@@ -12,25 +12,45 @@
  */
 var findAnagrams = function(s, p) {
   const sArr = new Array(26).fill(0)
-  const pArr = new Array(26).fill(0)
-  let arr = []
+  let finArr = []
+  let dif = 0
 
-  if(s.length < p.length) return arr
+  if(s.length < p.length) return finArr
 
   for(let i = 0; i < p.length; i++) {
     sArr[s[i].charCodeAt() - 'a'.charCodeAt()]++
-    pArr[p[i].charCodeAt() - 'a'.charCodeAt()]++
+    sArr[p[i].charCodeAt() - 'a'.charCodeAt()]--
   }
 
-  if(sArr.toString() === pArr.toString()) arr.push(0)
-
-  for(let i = p.length; i < s.length; i++) {
-    sArr[s[i - p.length].charCodeAt() - 'a'.charCodeAt()]--
-    sArr[s[i].charCodeAt() - 'a'.charCodeAt()]++
-    if(sArr.toString() === pArr.toString()) arr.push(i - p.length + 1)
+  for(let i = 0; i < 26; i++) {
+    if(sArr[i] !== 0) dif++
   }
 
-  return arr
+  if(dif === 0) finArr.push(0)
+
+  for(let i = 0; i < s.length - p.length; i++) {
+    // 滑动窗口左端点，将要移除，如果等于1的话，移除后刚好为0
+    if(sArr[s[i].charCodeAt() - 'a'.charCodeAt()] === 1) {
+      dif--
+    } else if(sArr[s[i].charCodeAt() - 'a'.charCodeAt()] === 0) {
+      // 滑动窗口左端点，将要移除，如果等于0的话，移除后不为0
+      dif++
+    }
+    sArr[s[i].charCodeAt() - 'a'.charCodeAt()]--
+    
+    // 滑动窗口右端点，将要添加，如果等于-1的话，添加后刚好为0
+    if(sArr[s[i + p.length].charCodeAt() - 'a'.charCodeAt()] === -1) {
+      dif--
+    } else if(sArr[s[i + p.length].charCodeAt() - 'a'.charCodeAt()] === 0) {
+      // 滑动窗口右端点，将要添加，如果等于0的话，添加后不为0
+      dif++
+    }
+    sArr[s[i + p.length].charCodeAt() - 'a'.charCodeAt()]++
+
+    if(dif === 0) finArr.push(i + 1)
+  }
+
+  return finArr
 };
 // @lc code=end
 
